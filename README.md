3be9f5e0eb71b13bd18cf0df60095cc3a26ed3d1
Julia Faqir <julia.faqir@pwc.com>
2023-03-27 09:39:58 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Task no. 7 
+#### REST APIs, event loop, middleware and error handling
+
+-------
+APIs endpoints:
+- Get courses : /api/courses
+- Get course by id: /api/courses/1
+- Post new course : /api/courses, and add the following in the body {"name": "course name"}
+- Put to update specific course: /api/courses/1, and add the new course name 
---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const app = express();
+const Joi = require('joi');
+
+app.use(express.json());
+
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'}
+]
+
+// PORT
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log('App listening on port ' + listener.address().port);
+  });
+
+//app.get()
+app.get('/', (req,res)=>{
+    res.send('<h1>Hello World!!!</h1>');
+});
+
+app.get('/api/courses', (req,res)=>{
+    res.send(courses);
+});
+
+
+app.get('/api/courses/:id', (req,res) =>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('course with given ID not found');
+    res.send(course);
+});
+
+// app.post()
+app.post('/api/courses', (req,res)=>{
+    const { error } = validateCourse(req.body); // eq to result.error
+    if(error) return res.status(400).send(result.error.details[0].message);
+  
+    const course = {
+        id: courses.length +1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+// app.put()
+app.put('/api/courses/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('course with given ID not found');
+
+    // const result = validateCourse(req.body);
+    const { error } = validateCourse(req.body); // eq to result.error
+    if(error) return res.status(400).send(result.error.details[0].message)
+        
+    //update course
+    course.name = req.body.name;
+    //return the updated course
+    res.send(course);
+})
+
+// app.delete()
+app.delete('/api/courses/:id', (req,res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('course with given ID not found');
+
+    const index = courses.indexOf(course);
+
+    courses.splice(index,1);
+
+    res.send(course);
+
+})
+
+//Validate fun
+
+function validateCourse(course){
+    const schema ={
+        name: Joi.string().min(3).required() 
+    };
+
+    return Joi.validate(course, schema);
+
+}
+
+
